02b11715af3026f8558bf2041451afcc2df90d46
waweroh <scadmsez@gmail.com>
2023-03-26 22:40:31 +0300

    create a calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[result, setResult] = useState("0")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App w-full h-full flex justify-center bg-cyan-300'>
+      <div className='w-1/3 h-1/3 bg-black text-blue-300 rounded-lg mt-12 p-2'>
+        <div className='text-right mr-12 mt-20 mb-10 text-3xl'>{result}</div>
+        <div className='grid grid-rows-4 grid-cols-4 gap-2'>
+        <button className='w-20 h-20 rounded-full bg-gray-500 text-gray-900 font-bold'>AC</button>
+          <button className='w-20 h-20 rounded-full bg-gray-500 text-gray-900 font-bold'>+/-</button>
+          <button className='w-20 h-20 rounded-full bg-gray-500 text-gray-900 font-bold'>%</button>
+          <button className='w-20 h-20 rounded-full bg-orange-500'>/</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>7</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>8</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>9</button>
+          <button className='w-20 h-20 rounded-full bg-orange-500'>x</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>4</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>5</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>6</button>
+          <button className='w-20 h-20 rounded-full bg-orange-500'>-</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>1</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>2</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>3</button>
+          <button className='w-20 h-20 rounded-full bg-orange-500'>+</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>0</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>turn</button>
+          <button className='w-20 h-20 rounded-full bg-gray-900'>.</button>
+          <button className='w-20 h-20 rounded-full bg-orange-500'>=</button>
+
+        </div>
+        
+      </div>
     </div>
+    
   );
 }
 
